b0cfd964aa48b1d0a7eb1a263cac0d2dd07d40a6
Gex <ashlyn444@hotmail.com>
2019-03-22 09:58:56 +0800

    woooosh added lil lil lil bit more tests

--- differentusers/testlogin/schedule/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Schedule
-
-# Create your views here.
-class ScheduleListView(DetailView):
-    template_name = "schedule/schedule_list.html"
-    queryset = Schedule.objects.all()
