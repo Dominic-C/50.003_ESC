a56ee25ba406dde5ae9f227bc5b70ea3ebc52c0e
Dominic-c <dominiccordeiro@gmail.com>
2019-03-22 10:01:06 +0800

    added form and view for schedule

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,8 +3,36 @@ from django.contrib import admin
 # Register your models here.
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death') # displaying from models.py
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')] # for formatting
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre') # displaying from models.py. cannot display many to many field like genre, so we need to def a function
+    inlines = [BookInstanceInline] # displays all the book's instances from the bookinstances class
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', ('id', 'imprint'))
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+# @admin.register(BookInstance)
