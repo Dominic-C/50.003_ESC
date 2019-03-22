0945dfcee3f8602bb533513716ec828a3df57066
Dominic-c <dominiccordeiro@gmail.com>
2019-03-22 17:13:23 +0800

    modified schedule forms.py

--- differentusers/testlogin/schedule/forms.py
+++ differentusers/testlogin/schedule/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from schedule.models import Schedule
 from loginpage.models import User
+import datetime as dt
+
+HOUR_CHOICES = [(dt.time(hour=x), '{:02d}:00'.format(x)) for x in range(0, 24)]
 
 class CreateSchedule(forms.ModelForm):
 
@@ -9,3 +12,6 @@ class CreateSchedule(forms.ModelForm):
     class Meta:
         model = Schedule
         fields = ['title','description','start_time','end_time', 'lecturer', 'location']
+        widgets = {'start_time': forms.Select(choices=HOUR_CHOICES),
+        'end_time':forms.Select(choices=HOUR_CHOICES)
+        }
