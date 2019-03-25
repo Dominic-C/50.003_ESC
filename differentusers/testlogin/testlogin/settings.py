a2159b0607b216b822247026121c41e17c468a9e
Gex <ashlyn444@hotmail.com>
2019-03-25 13:48:26 +0800

    Implemented Lockout
    repeated login attempts with wrong passwords would lock user out. Used django-brutebuster

--- differentusers/testlogin/testlogin/settings.py
+++ differentusers/testlogin/testlogin/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
 
     'crispy_forms',
+    'BruteBuster',
 
     'loginpage',
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'BruteBuster.middleware.RequestMiddleware',
 ]
 
 ROOT_URLCONF = 'testlogin.urls'
