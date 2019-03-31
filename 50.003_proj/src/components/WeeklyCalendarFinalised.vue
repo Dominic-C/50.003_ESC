5ffea3d2dec2da2596f8462cc8924585f4e62713
Rachel <rachelchuajy@yahoo.com.sg>
2019-03-31 18:24:46 +0800

    migrating changes from calendarPage

---
+++ 50.003_proj/src/components/WeeklyCalendarFinalised.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- <div class="ds-expand ds-calendar-app">
+   -->
+    <ds-weekly-calendar :events="events" :calendar="calendar" :read-only="readOnly">
+      <template slot="eventDetailsExtra" slot-scope="{ details }">
+        <v-text-field 
+          single-line hide-details solo flat
+          prepend-icon="school"
+          label="Professor"
+          :readonly="readOnly"
+          v-model="details.professor"
+        >{{ details }}</v-text-field>
+        <v-text-field 
+          single-line hide-details solo flat
+          prepend-icon="group"
+          label="Class enrolled"
+          :readonly="readOnly"
+          v-model="details.classEnrolled"
+        ></v-text-field>
+      </template>
+    </ds-weekly-calendar>
+  <!-- </div> -->
+</template>
+
+<script>
+import { Calendar } from 'dayspan';
+import dsWeeklyCalendar from '../components/DaySpanWeeklyCalendar.vue';
+export default {
+    name: 'weeklyCalendarFinalised',
+    props: {
+      events: {
+          type: Array
+      }
+    },
+    components: {
+      dsWeeklyCalendar
+    },
+    data: vm => ({
+      readOnly: true,
+      calendar: Calendar.weeks()
+    })
+}
+</script>
