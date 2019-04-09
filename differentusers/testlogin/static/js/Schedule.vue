615656aee70043bc5afa612139dce4f82a4f82de
Lee Gui An <guilee186@gmail.com>
2019-04-10 01:59:52 +0800

    Import backbone of Vue calendar

---
+++ differentusers/testlogin/static/js/Schedule.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="schedule">
+    <v-app id="dayspan" v-cloak>
+      <v-content>
+        <search-bar
+          :calendarEventsTable="calendarEventTable"
+          :professorTable="professorTable"
+          :courseNameTable="courseNameTable"
+          :locationTable="locationTable"
+          :classTable="classTable"
+          @selected-search-item="updateCalendar"
+        ></search-bar>
+        <list-selection :courseList="courseTable"></list-selection>
+        <weekly-calendar-finalised :events="selectedCalendarEvents">></weekly-calendar-finalised>
+        <weekly-calendar-suggestable :events="selectedCalendarEvents">></weekly-calendar-suggestable>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+
+<script>
+import { Calendar, Weekday } from "dayspan";
+import Colors from "dayspan-vuetify/src/colors.js";
+import SearchBar from "./components/SearchBar.vue";
+import ListSelection from "./components/ListSelection.vue";
+import weeklyCalendarSuggestable from "./components/WeeklyCalendarSuggestable.vue";
+
+export default {
+  name: "scheduler",
+  data: () => ({})
+};
+</script>
